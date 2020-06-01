b3bbf0d60264558e1250262c600e60989e399daf
Deepali Jain <61299175+deej17@users.noreply.github.com>
2020-06-01 17:59:42 -0400

    Add files via upload

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+### Schema
+
+CREATE DATABASE burger_db;
+
+USE burger_db;
+DROP table burgers;
+
+USE burger_db;
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT true,
+	PRIMARY KEY (id)
+);
